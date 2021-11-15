231b4db719f2558b20c7f288419bb73f42b9a867
Hermino Junior <herminojunior@gmail.com>
2021-11-15 11:37:36 -0400

    Delete file

--- configuration.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__, static_folder='static')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://desafio:12345678@127.0.0.1/teste'
-db = SQLAlchemy(app)
